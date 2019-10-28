27d5f8974c039272d9aaea4b1686921d751eb39e
Alex Zadorozhnyi <alexand98@yandex.ru>
2019-10-28 20:31:09 +0300

    add src/

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/utils/vars.scss'
+import './assets/scss/main.scss'
+import './pug/pages/colors-and-type/colors-and-type.scss'
+import './pug/pages/headers-and-footers/headers-and-footers.scss'
+
+
+// CSS (example)
+//import './assets/css/main.css'
+
+// Vue.js
+//window.Vue = require('vue')
+
+// Vue components (for use in html)
+//Vue.component('example-component', require('./components/Example.vue').default)
+
+// Vue init
+//const app = new Vue({
+//  el: '#app'
+//})
